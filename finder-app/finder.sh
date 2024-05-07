b95b6ff999bae3b1ca9a598d1455240fac30c5fd
Yasser Morsy <54084895+YKMorsy@users.noreply.github.com>
2024-05-07 13:06:05 -0700

    Create finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+
+        echo "Usage: $0 <filesdir> <searchstr>"
+        exit 1
+
+fi
+
+
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d "$filesdir" ]; then
+
+        echo "Directory does not exist"
+        exit 1
+
+fi
+
+num_lines=$(grep -r "$searchstr" $filesdir | wc -l)
+num_files=$(find $filesdir -type f | wc -l)
+
+# print count of expr
+echo "The number of files are $num_files and the number of matching lines are $num_lines"
